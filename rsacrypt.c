09adbdc5e2292022b1b239a9d7e59cd77bad7496
David Durán <davidduran94@gmail.com>
2015-07-13 00:18:32 -0500

    Manual tecnico

--- rsacrypt.c
+++ rsacrypt.c
@@ -5,13 +5,10 @@
 #include "rsa.h"
 
 int main(int argc,char *argv[]){
-	double a, base,res;
-	int n;//=33;
+	double a = 3, base = 29,res;
+	int n=33;
 	if(argc > 3){
-		printf("Nombre del programa %s",argv[0]);
-		sscanf(argv[1], "%lf", &a);
-		sscanf(argv[2], "%lf", &base);
-		n = atoi(argv[3]);
+		printf("Exito %s",argv[1]);
 		res = Power(a,base,n);
 		printf("Resultado %.2f\n", res);
 	}
